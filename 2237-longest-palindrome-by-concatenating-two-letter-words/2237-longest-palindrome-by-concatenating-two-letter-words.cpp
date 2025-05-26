55ae1071bfc1e787e2341b625dad1629f8c396f9
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2025-05-26 17:32:00 +0530

    Time: 32 ms (80.75%), Space: 172.2 MB (20.64%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -3,19 +3,19 @@ public:
     int longestPalindrome(vector<string>& words) {
         int count1 = 0, n = words.size(), count2 = 0;
         string str;
-        unordered_map<string, int> mp;
+        vector<vector<int>> mp(26, vector<int>(26));
         for(int i = 0; i < n; i++) {
             str = words[i];
             swap(str[0], str[1]);
             if(str == words[i]) {
-                if(!(mp[str] % 2)) count2++;
+                if(!(mp[str[0] - 'a'][str[1] - 'a'] % 2)) count2++;
                 else count2--;
             }
-            if(mp[str] > 0) {
+            if(mp[str[0] - 'a'][str[1] - 'a'] > 0) {
                 count1 += 4;
-                mp[str]--;
+                mp[str[0] - 'a'][str[1] - 'a']--;
             }
-            else mp[words[i]]++;
+            else mp[words[i][0] - 'a'][words[i][1] - 'a']++;
         }
         return count1 + ((count2 > 0) ? 2 : 0);
     }
