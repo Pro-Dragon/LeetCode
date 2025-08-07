1e6f41ee67550abb87db5f5f3743f0f0e80a8241
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2025-08-07 20:44:16 +0530

    Time: 0 ms (100%), Space: 115.7 MB (56.81%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -6,12 +6,17 @@ public:
         res.push_back(nums[0]);
         for(int i = 1; i < n; i++) {
             if(res.back() < nums[i]) {
+                if(res.size() == 2) return 1;
                 res.push_back(nums[i]);
-                if(res.size() == 3) return 1;
             }
             else {
-                auto index = std::lower_bound(res.begin(), res.end(), nums[i]) - res.begin();
-                res[index] = nums[i];
+                if(res.size() == 2) {
+                    if(nums[i] > res[0]) res[1] = nums[i];
+                    else res[0] = nums[i];
+                }
+                else {
+                    res[0] = nums[i];
+                }
             }
         }
         return 0;
