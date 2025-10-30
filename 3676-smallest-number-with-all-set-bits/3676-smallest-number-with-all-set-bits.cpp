0831aa5a92e8fb8f12d4e2319c9357906050756b
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2025-10-30 23:20:04 +0530

    Time: 0 ms (100%), Space: 8.3 MB (30.05%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int res = 0, i = 0;
-        while(n >> i) {
-            res += ((1 << (i)));
-            i++; 
-        }
-        return res;
+        return (pow(2,floor(log2(n))+1))-1;
     }
 };
