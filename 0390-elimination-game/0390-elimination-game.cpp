0c8cae9cf5429949fe2c33d9995c4b7d4c9bd7cb
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-02-26 15:12:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (77.31%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        long long val = 1, diff = 1, size = n;
-        for(int i = 0; val + diff <= n; i++) {
-            if(!(i % 2) || (size % 2)) val = val + diff;
-            diff = (long)diff * 2;
-            size = (size / 2); 
+        int diff = 1, val = 1;
+        for(int i = 1; n / 2; i++) {
+            if(n % 2 || i % 2) val += diff;
+            diff *= 2;
+            n /= 2;
         }
         return val;
     }
