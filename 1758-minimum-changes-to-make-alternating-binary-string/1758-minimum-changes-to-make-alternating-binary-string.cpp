cc469058c9830e8675ed0f37dd32f66ce6b16297
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-03-05 11:53:17 +0530

    Time: 0 ms (100%), Space: 9.6 MB (20.96%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int count1 = 0, count2 = 0;
+        int count = 0, n = s.size();
         for(int i = 0; s[i]; i++) {
-            if(i % 2 == s[i] - '0') count1++;
-            else count2++;
+            if(i % 2 == s[i] - '0') count++;
         }
-        return min(count1, count2);
+        return min(count, n - count);
     }
 };
