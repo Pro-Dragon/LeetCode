91bf0c820ba3d14940a1666a1881b146aa34ae87
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-03-26 22:05:24 +0530

    Time: 4 ms (68.01%), Space: 165.9 MB (65.6%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int odd = 0, even = 0, n = nums1.size(), mini = nums1[0];
+        int odd = 0, n = nums1.size(), mini = nums1[0];
         for(int i = 0; i < n; i++) {
             if(nums1[i] % 2) odd++;
-            else even++;
             mini = min(mini, nums1[i]);
         }
         return (mini % 2 == 0 && odd) ? 0 : 1;
