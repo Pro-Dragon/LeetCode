54d2e9fde25bf2f1479eacea4c596985f5c4ae3c
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-05-12 15:54:05 +0530

    Time: 35 ms (71.84%), Space: 97.5 MB (39.81%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -6,12 +6,6 @@ public:
             return (a[1] - a[0]) > (b[1] - b[0]);
         });
         for(int i = 0; i < n; i++) {
-            res += tasks[i][0];
-            maxi = max(maxi, tasks[i][1]);
-        }
-        res = max(res, maxi);
-        maxi = res;
-        for(int i = 0; i < n; i++) {
             if(tasks[i][1] <= maxi) maxi -= tasks[i][0];
             else {
                 res += (tasks[i][1] - maxi);
