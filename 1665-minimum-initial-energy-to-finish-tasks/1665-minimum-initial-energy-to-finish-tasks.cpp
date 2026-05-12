9d55d38f516babad5be9903df3a3e5a165c831fa
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-05-12 15:58:15 +0530

    Time: 31 ms (83.98%), Space: 97.2 MB (85.92%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
         int n = tasks.size(), res = 0, maxi = 0;
-        sort(tasks.begin(), tasks.end(), [](vector<int> &a, vector<int> &b){
+        sort(tasks.begin(), tasks.end(), [&](vector<int> &a, vector<int> &b){
             return (a[1] - a[0]) > (b[1] - b[0]);
         });
         for(int i = 0; i < n; i++) {
