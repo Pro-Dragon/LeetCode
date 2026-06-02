288f1a745ff6f985da2f0818f10813084e51ec96
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-06-02 16:38:55 +0530

    Time: 2 ms (80%), Space: 238 MB (93.33%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -2,43 +2,15 @@ class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
         int landstart = 0, landend = INT_MAX, waterstart, waterend = INT_MAX, n = landStartTime.size(), m = waterStartTime.size(), res = INT_MAX;
-        for(int i = 0; i < n; i++) {
-            if(landend >= landStartTime[i] + landDuration[i]) {
-                if(landend == landStartTime[i] + landDuration[i]) {
-                    if(landstart < landStartTime[i]) landstart = landStartTime[i]; 
-                }
-                else landstart = landStartTime[i];
-                landend = landStartTime[i] + landDuration[i];
-            }
-        }
+        for(int i = 0; i < n; i++) landend = min(landend, landStartTime[i] + landDuration[i]);
         for(int i = 0; i < m; i++) {
-            if(waterend >= waterStartTime[i] + waterDuration[i]) {
-                if(waterend == waterStartTime[i] + waterDuration[i]) {
-                    if(waterstart < waterStartTime[i]) waterstart = waterStartTime[i]; 
-                }
-                else waterstart = waterStartTime[i];
-                waterend = waterStartTime[i] + waterDuration[i];
-            }
-            if(waterStartTime[i] < landstart) {
-                if(landstart < waterStartTime[i] + waterDuration[i]) res = min(res, waterStartTime[i] + waterDuration[i] + (landend - landstart));
-                else res = min(res, landend);
-            }
-            else if(waterStartTime[i] > landstart) {
-                if(waterStartTime[i] < landend) res = min(res, waterDuration[i] + landend);
-                else res = min(res, waterStartTime[i] + waterDuration[i]);
-            }
-            else res = min(res, landend + waterDuration[i]);
+            waterend = min(waterend, waterStartTime[i] + waterDuration[i]);
+            if(landend > waterStartTime[i]) res = min(res, landend + waterDuration[i]);
+            else res = min(res, waterStartTime[i] + waterDuration[i]);
         }
         for(int i = 0; i < n; i++) {
-            if(landStartTime[i] < waterstart) {
-                if(waterstart < landStartTime[i] + landDuration[i]) res = min(res, landStartTime[i] + landDuration[i] + (waterend - waterstart));
-                else res = min(res, waterend);
-            }
-            else if(landStartTime[i] > waterstart) {
-                if(landStartTime[i] < waterend) res = min(res, landDuration[i] + waterend);
-                else res = min(res, landStartTime[i] + landDuration[i]);
-            }
-            else res = min(res, waterend + landDuration[i]);
+            if(waterend > landStartTime[i]) res = min(res, waterend + landDuration[i]);
+            else res = min(res, landStartTime[i] + landDuration[i]);
         }
         return res;
     }
