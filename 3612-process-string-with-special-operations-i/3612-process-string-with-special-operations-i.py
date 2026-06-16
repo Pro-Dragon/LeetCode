ad914bee71719c9fcc8bdef8978339dff63d807d
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-06-16 18:24:24 +0530

    Time: 35 ms (67.11%), Space: 26 MB (44.08%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        stack = []
+        for i in range(len(s)):
+            if s[i] in letters:
+                stack.append(s[i])
+            elif s[i] == '#':
+                if stack:
+                    stack.extend(stack)
+            elif s[i] == '%':
+                stack = stack[::-1]
+            elif s[i] == '*':
+                if len(stack) == 0:
+                    continue
+                stack.pop()
+        res = "".join(stack)
+        return res
