4126fdadc21f17e904de980e3528ce7cbf0e6167
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-07-06 16:30:10 +0530

    Time: 0 ms (100%), Space: 10.7 MB (42%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int n = arr.size(), cur = 0, res = 0, pre = 0;
+        int n = arr.size(), res = 0, pre = 0;
         unordered_map<int, int> mp1, mp2;
-        for(int i = 0; i < n; i++) {
-            cur ^= arr[i];
-            mp1[cur]++;
-           // cout<<cur<<" ";
-        }
-        //cout<<endl;
-        cur = 0, pre = 0;
-        //res += cur;
-        mp2[0] = 1;
+        pre = 0;
+        mp2[0] = 0;
         mp1[0]++;
         for(int i = 0; i < n; i++) { 
             pre ^= arr[i];
-            mp2[pre]++;
-            cur -= (mp2[pre] - 1);
-            if(mp1[pre ^ arr[i]]) {
-                cur += mp1[pre ^ arr[i]] - mp2[pre ^ arr[i]];
-                //cout<<mp2[pre]<<endl<<endl;
-            }
-            res += cur;
-            //cout << "Index: " << i << " | Pre: " << pre << " | Cur: " << cur << endl;
+            res += mp1[pre]++ * i - mp2[pre];
+            mp2[pre] += (i + 1);
         }
+        cout<<mp1[2];
         return res;
     }
 };
