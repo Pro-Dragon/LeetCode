14b0047f13f3e4057e6eaf9a86b6040b3eebc43f
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-07-15 22:57:47 +0530

    Time: 106 ms (5.11%), Space: 31.3 MB (81.51%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        unordered_map<int, int> mp;
+        int n = hand.size(), cur = 0, max_ele = -1;
+        for(int i = 0; i < n; i++) {
+            mp[hand[i]]++;
+        }
+        for(int i = 0; i < n; i++) {
+            if(!mp[hand[i] - 1] && hand[i] > max_ele) {
+                cur = 0;
+                for(int j = hand[i]; mp[j] > 0; j++) {
+                    if(cur > mp[j]) return 0;
+                    mp[j] -= cur; 
+                    if(j - hand[i] >= groupSize - 1) {
+                        cur -= mp[j - (groupSize - 1)];
+                    }
+                    cur += mp[j];
+                    cout<<cur<<" "<<j<<endl;
+                    max_ele = j;
+                }
+                if(cur) return 0;
+            }
+        }
+        return 1;
+    }
+};
