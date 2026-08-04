531a4698b59116605535fc45dc065bf6b8c5556d
DHANUSH NAGISETTI <147791825+Pro-Dragon@users.noreply.github.com>
2026-08-04 13:36:09 +0530

    Time: 21 ms (96.87%), Space: 115.7 MB (64.82%) - LeetHub

--- 1686-stone-game-vi/1686-stone-game-vi.cpp
+++ 1686-stone-game-vi/1686-stone-game-vi.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
-        priority_queue<pair<int, int>> pq;
+        vector<vector<int>> freq(201);
         int n = aliceValues.size(), a = 0, b = 0, turn = true;
         for(int i = 0; i < n; i++) {
-            pq.push({aliceValues[i] + bobValues[i], i});
+            freq[aliceValues[i] + bobValues[i]].push_back(i);
         }
-        while(!pq.empty()) {
-            if(turn) a += aliceValues[pq.top().second];
-            else b += bobValues[pq.top().second];
-            turn = !turn;
-            pq.pop();
+        for(int i = 200; i >= 0; i--) {
+            for(int j = 0; j < freq[i].size(); j++) {
+                if(!turn) {
+                    b += bobValues[freq[i][j]];
+                }
+                else {
+                    a += aliceValues[freq[i][j]];
+                }
+                turn = !turn;
+            }
         }
         if(a > b) return 1;
         if(b > a) return -1;
